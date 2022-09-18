21f3a736e65afcb782a4d322cf1f097952c3b91c
BK <1209157041@qq.com>
2022-09-18 22:01:58 +0800

    初始化aside 的html

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-<div>hi</div>
-<Bk></Bk>
+
+<router-view />
 </template>
 
 <script>
-import Bk from './components/Bk.vue';
+
 
 export default {
   name: 'App',
-  components: {
-   Bk
-  }
+
 }
 </script>
