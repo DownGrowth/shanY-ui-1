40c70a5560a1beca4f93b0672dc7089429ba24f4
BK <1209157041@qq.com>
2022-09-19 17:48:18 +0800

    优化css

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -26,11 +26,10 @@ button{
     height: $h;
     width: $h*2;
     border:none;
-    background: grey;
+    background: #bfbfbf;
     border-radius: $h/2;
     position: relative;
-}
-span{
+    > span{
     position: absolute;
     top: 2px;
     left: 2px;
@@ -38,15 +37,29 @@ span{
     width: $h2;
     background: white;
     border-radius: $h2/2;
-    transition: left 250ms;
+    transition: all 250ms;
 }
-button.checked{
-    background: blue;
+&.checked{
+    background: #1890ff;
+    > span{
+        left:calc(100% - #{$h2} - 2px)
+    }
 }
-button.checked > span{
-    left:calc(100% - #{$h2} - 2px) ;
+&:focus{
+    outline: none;
 }
-button:focus{
-  outline: none;
+&:active{
+    > span{
+        width: $h2+4px;
+    }
 }
+&.checked:active{
+    > span{
+        width: $h2 +4px;
+        margin-left: -4px;
+    }
+}
+}
+
+
 </style>
--- src/view/Doc.vue
+++ src/view/Doc.vue
@@ -84,4 +84,5 @@ export default {
 main {
   overflow: auto;
   }
+
 </style>
