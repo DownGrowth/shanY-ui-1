84c5f9dfc26eacea5f06b6b5c6dd016dc52ea74f
BK <1209157041@qq.com>
2022-09-19 17:18:06 +0800

    使用v-model

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
-        Switch 示例
+        <Switch v-model:value="y" />
     </div>
 </template>
+<script lang="ts">
+import Switch from '../lib/Switch.vue';
+import { ref } from 'vue';
+export default {
+    components: { Switch },
+    setup() {
+        const y = ref(true)
+        return {y}
+    }
+}
+</script> 
