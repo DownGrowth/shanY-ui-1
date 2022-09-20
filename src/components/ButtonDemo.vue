9feab202222c8e1b7a39bcd937d69a6e8a97a25d
BK <1209157041@qq.com>
2022-09-20 19:04:18 +0800

    基本完善 button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -47,7 +47,8 @@
        </div>
        <h1>示例5</h1>
        <div>
-        <Button loading>加载</Button>
+        <Button loading>加载中</Button>
+        <Button>加载完毕</Button>
        </div>
     </div>
 </template>
