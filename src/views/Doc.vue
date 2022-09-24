5a3a7ed30804af3cba7d06bc6111b75ec46a57f4
BK <1209157041@qq.com>
2022-09-24 16:31:43 +0800

    创建文档

---
+++ src/views/Doc.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="layout">
+    <Topnav :toggleMenuButtonVisible="true" class="nav"/>
+    <div class="content">
+      <aside v-if="asideVisible">
+        <h2>文档</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/intro">介绍</router-link>
+          </li>
+        
+          <li>
+            <router-link to="/doc/install">安装</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/get-started">开始使用</router-link>
+          </li>
+        </ol>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
+      </aside>
+      <main>
+        <router-view/>
+      </main>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { inject, Ref } from 'vue';
+import Topnav from '../components/Topnav.vue'
+export default {
+components: { Topnav },
+setup() {
+  const asideVisible = inject<Ref<boolean>>('asideVisible')
+  return {asideVisible}
+ }     
+  }
+</script>
+<style lang="scss" scoped>
+.layout {
+display: flex;
+flex-direction: column;
+height: 100vh;
+> .content {
+  flex-grow: 1;
+  padding-top: 60px;
+  padding-left: 156px;
+  @media (max-width: 500px) {
+    padding-left: 0; 
+  }
+}
+}
+.content {
+display: flex;
+> aside {
+  flex-shrink: 0;
+  z-index: 10;
+}
+> main {
+  flex-grow: 1;
+  padding: 16px;
+  background: rgb(241, 247, 244);
+}
+}
+aside{
+  background:rgb(243, 243, 243);
+  width:150px;
+  padding:16px;
+  position: fixed;
+top: 0;
+left: 0;
+padding-top: 70px;
+height: 100%;
+  
+  >h2{
+    margin-bottom:4px;
+  }
+  >ol{
+    >li{
+      padding:4px 0;
+    }
+  }
+}
+main {
+overflow: auto;
+}
+</style>
